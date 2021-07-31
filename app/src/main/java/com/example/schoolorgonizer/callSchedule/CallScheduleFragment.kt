071b708de5220be860bdf1cc021cf92d52e18fd2
User <https://github.com/Vla-a/Mr-jd2-78-21.git>
2021-07-31 16:09:22 +0300

    fix Call Schedule Fragment

--- app/src/main/java/com/example/schoolorgonizer/callSchedule/CallScheduleFragment.kt
+++ app/src/main/java/com/example/schoolorgonizer/callSchedule/CallScheduleFragment.kt
@@ -1,25 +1,54 @@
 package com.example.schoolorgonizer.callSchedule
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.schoolorgonizer.R
+import com.example.schoolorgonizer.databinding.FragmentCallScheduleBinding
 
 
 class CallScheduleFragment : Fragment() {
 
+    private lateinit var binding: FragmentCallScheduleBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return         inflater.inflate(R.layout.fragment_call_schedule, container, false)
+                binding = FragmentCallScheduleBinding.inflate(inflater, container, false)
+        return binding?.root
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ObjectAnimator.ofFloat(binding?.nvText1,View.TRANSLATION_X, 800f, 0f).apply {
+            duration = 1000
+        }.start()
+        ObjectAnimator.ofFloat(binding?.nvText2, View.TRANSLATION_Y, 800f, 0f).apply {
+            duration = 2000
+        }.start()
+        ObjectAnimator.ofFloat(binding?.nvText3,  View.TRANSLATION_X, 800f, 0f).apply {
+            duration = 1000
+        }.start()
+        ObjectAnimator.ofFloat(binding?.nvText4, View.TRANSLATION_Y, 800f, 0f).apply {
+            duration = 2000
+        }.start()
+        ObjectAnimator.ofFloat(binding?.nvText5, View.TRANSLATION_X, 800f, 0f).apply {
+            duration = 1000
+        }.start()
+        ObjectAnimator.ofFloat(binding?.nvText6, View.TRANSLATION_Y, 800f, 0f).apply {
+            duration = 2000
+        }.start()
+        ObjectAnimator.ofFloat(binding?.nvText7, View.TRANSLATION_X, 800f, 0f).apply {
+            duration = 1000
+        }.start()
+    }
     companion object {
         const val TAG = "CallScheduleFragment"
 
